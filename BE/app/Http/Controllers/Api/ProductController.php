ffc630cc76695b40fabe35b15ee37779789099f6
Hoàng <phuongminhhoang77@gmail.com>
2024-12-28 08:54:37 +0700

    Done create

--- BE/app/Http/Controllers/Api/ProductController.php
+++ BE/app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -18,9 +20,28 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'product_name' => 'required|string|max:255',
+            'product_thumbnail' => 'nullable|image', 
+            'product_price' => 'required|numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $data = $request->except('product_thumbnail');
+
+        if ($request->hasFile('product_thumbnail')) {
+            $path_product_thumbnail = $request->file('product_thumbnail')->store('product_thumbnails');
+            $data['product_thumbnail'] =  $path_product_thumbnail;
+        }
+
+        $product = Product::create($data); 
+
+        return response()->json($product, 201);
     }
 
     /**
