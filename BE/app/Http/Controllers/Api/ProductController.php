78e5bcd5746ddefd029824e6e62903fb74b96534
tuduong212 <dtu52717@gmail.com>
2024-12-28 19:18:56 +0700

    New Update

--- BE/app/Http/Controllers/Api/ProductController.php
+++ BE/app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -28,15 +31,59 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $data = $request->except('product_thumbnail');
+
+        $validator = Validator::make($data, [
+            'product_name' => ['required', 'string', 'max:255'],
+            'product_thumbnail' => ['nullable', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
+            'product_price' => ['required', 'numeric', 'min:0']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'message' => 'Failed',
+                    'errors' => $validator->errors()
+                ],
+                422
+            );
+        }
+
+        try {
+
+            $product = Product::query()->findOrFail($product->id);
+
+            $path = '';
+
+            if ($request->hasFile('product_thumbnail')) {
+
+                $path = $request->file('product_thumbnail')->store('product_thumbnails');
+                $data['product_thumbnail'] = $path;
+                Storage::delete($product->product_thumbnail);
+
+            } else {
+                $data['product_thumbnail'] = $product->product_thumbnail;
+            }
+
+            $product->update($data);
+
+            return response()->json([
+                'message' => 'Cập nhật dữ liệu thành công',
+                'products' => $product
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Cập nhật dữ liệu thất bại',
+            ], 500);
+        }
     }
 
     /**
--- BE/app/Http/Controllers/ProductController.php
+++ BE/app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -36,16 +38,17 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
-    }
 
+    }
+    
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(Product $product)
     {
         $product->find($product->id);
-        return view('',compact('product'));
+        // return view('', compact('product'));
+        return $product;
     }
 
     /**
@@ -53,10 +56,42 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        $data = $request->all();
+        $data = $request->except('product_thumbnail');
+
+        $validator = Validator::make($data, [
+            'product_name' => ['required', 'string', 'max:255'],
+            'product_thumbnail' => ['nullable', 'image', 'mimes:jpg,jpeg,png', 'max:2048'],
+            'product_price' => ['required', 'numeric', 'min:0']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'message' => 'Failed',
+                    'errors' => $validator->errors()
+                ],
+                422
+            );
+        }
+
+        $path = '';
+
+        if ($request->hasFile('product_thumbnail')) {
+
+            $path = $request->files('product_thumbnail')->store('product_thumbnails');
+            $data['product_thumbnail'] = $path;
+
+            Storage::delete($product->product_thumbnail);
+
+        } else {
+            $data['product_thumbnail'] = $product->product_thumbnail;
+        }
 
         $product->update($data);
-        return redirect()->back()->with('messageUpdate', 'Cập nhật dữ liệu thành công');
+
+        // return redirect()->back()->with('messageUpdate', 'Cập nhật dữ liệu thành công');
+
+        return $product;
     }
 
     /**
